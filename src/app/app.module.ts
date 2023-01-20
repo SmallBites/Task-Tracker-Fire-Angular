0b6c65d4326bae21de19fed13ce9cafbc9f5cae5
whooperdars <kimdariuspanis@gmail.com>
2023-01-21 06:12:01 +0800

    Merge conflix resolved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
-import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from 'environments/environment';
+
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
+
+const MaterialImports = [];
+const PrimeImports = [];
 
 @NgModule({
   declarations: [AppComponent],
